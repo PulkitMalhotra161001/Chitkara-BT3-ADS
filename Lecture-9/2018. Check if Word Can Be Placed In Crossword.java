3695543983206b924c2d2f884685609240cea285
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2024-03-18 19:24:35 +0530

    Update 2018. Check if Word Can Be Placed In Crossword.java

--- Lecture-9/2018. Check if Word Can Be Placed In Crossword.java
+++ Lecture-9/2018. Check if Word Can Be Placed In Crossword.java
@@ -1,28 +1,25 @@
 class Solution {
     
-    //keep in mind this is just a small part of a problem
-    //we are having only one word which we have to find
-    //if it can be placed inside board or not(not returing grid after filling the word)
-
     public boolean placeWordInCrossword(char[][] board, String word) {
-    
+        int n=board.length, m=board[0].length, wn=word.length();
+
         //iterate in the board
-        for(int i=0;i<board.length;i++) {
-			for(int j=0;j<board[0].length;j++) {
+        for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
         
                 //check if character and space present only then go to inside if
 				if(board[i][j]!='#') {
                 
-                    //can we put this word downward,rightward,upward,leftward
-                    //sequence doesn't matter we are just checking
-                    //if we find anywhere
-                    //means we can fit this word somewhere in the board.
-                    //So, return true
-					if(canputdown(board,word,i,j))  return true;
-					if(canputright(board,word,i,j)) return true;
-                    if(canputup(board,word,i,j))    return true;
+                    //before going check if it is a valid start
                     
-                    if(canputleft(board,word,i,j))  return true;
+                    if( validEnd(board,i+1,j,n,m) && canPut(board,word,0,wn,i,j,n,m,'U') )
+                        return true;
+                    if( validEnd(board,i-1,j,n,m) && canPut(board,word,0,wn,i,j,n,m,'D') ) 
+                        return true;
+                    if( validEnd(board,i,j+1,n,m) && canPut(board,word,0,wn,i,j,n,m,'L') ) 
+                        return true;
+                    if( validEnd(board,i,j-1,n,m) && canPut(board,word,0,wn,i,j,n,m,'R') )  
+                        return true;
 				}
 			}
 		}
@@ -31,141 +28,50 @@ class Solution {
         return false;
     }
     
-    
-	private static boolean canputright(char[][] a, String word, int i, int j) {
+    boolean canPut(char[][] a,String word,int wi,int wn,int i,int j,int n,int m,char dir) {
+        //put all char
+        if(wi==wn){
+            return validEnd(a,i,j,n,m);
+        }
         
-        //we are going rightward
-        //we have 2 cases where word can be fitted
-        //if we have don't have left side or we have #
-        //means we can't fill the word in front of any word and after spaces
-		//now we know the conditions so we have to inverse it
-        //means we have left space and there # not present (if that make sense)
-        if(j-1>=0 && a[i][j-1]!='#')
-			return false;
-    
-        //same goes for after the word
-        //we have nothing or # present
-        //so we inversed. We have after (adding the length of word) space left
-        //means we have to # after that. if both contions fail then we can't put word here
-		
-        else if(j+word.length()<=a[0].length-1 && a[i][j+word.length()]!='#')
-			return false;
-		
-        //now we are sure we can put word here
-		for(int jj=0;jj<word.length();jj++) {
-            //look bottom condition first
-            //if we are checking values and all values matched or we are having long string which can't place here
-		
-            if(j+jj>a[0].length-1)
-				return false;
-            //if we have space means we can put word here
-            //or we have element present which is same as index of our word element means we can put here
-            //here jj is used to moving leftward direction
-			//and i and j are index from where we are checking
-            
-            if(a[i][j+jj]==' ' || word.charAt(jj)==a[i][j+jj]) {
-				continue;
-			}
-			
-            else
-                //if we don't have space means we have to same word but we have not
-                //so we can't place word here
-				return false;
-		}
+        if(!valid(a,i,j,n,m,word,wi))   return false;
         
-        //everything works fine so we can place word here
-		return true;
-	}
-    
-    
-	//same as canputright method
-	private static boolean canputdown(char[][] a, String word, int i, int j) {
-	
-        //checking for downward direction
-        //so, if present area present(inside the box) so it should be #
-        //and it is not so we return false
-        if(i-1>=0 && a[i-1][j]!='#')
-			return false;
+        int ni,nj;
+        if(dir=='L'){
+            ni=i;
+            nj=j-1;
+        }else if(dir=='R'){
+            ni=i;
+            nj=j+1;
+        }else if(dir=='U'){
+            ni=i-1;
+            nj=j;
+        }else{
+            ni=i+1;
+            nj=j;
+        }
         
-        //same goes for this, checking after (adding the word/string length)
-		else if(i+word.length()<=a.length-1 && a[i+word.length()][j]!='#')
-			return false;
-		
-        for(int ii=0;ii<word.length();ii++) {
-			if(i+ii>a.length-1)
-				return false;
+        char temp = a[i][j];
         
-            //same as the previous one
-            //here ii is used for going downward direction
-			if(a[i+ii][j]==' ' || word.charAt(ii)==a[i+ii][j]) {
-				continue;
-			}
-			
-            else
-				return false;
-		}
+        a[i][j]=word.charAt(wi);
         
-        //everything works fine
-		return true;
-		
-	}
-    
-    
-    //here comes the fun part
-    private static boolean canputleft(char[][] a, String word, int i, int j) {
-	
-        //going left so we don't have to right index if we have it must be #
-        //if both condition fails means somthing other than # is present or index(i,j) is not the last the last index of grid
-        if(j+1<=a[0].length-1 && a[i][j+1]!='#')
-			return false;
+        if(canPut(a,word,wi+1,wn,ni,nj,n,m,dir))    return true;
         
-        //checking if string can be fit here
-        //keep in mind we are going right hand to left hand side
-		else if(j-word.length()>=0 && a[i][j-word.length()]!='#')
-			return false;
-		
-        for(int jj=0;jj<word.length();jj++) {
-			//i think you got this part already
-            //yes! we are out of the grid if this is the case means we are having long string that can't be places here
+        a[i][j]=temp;
         
-            if(j-jj<0)
-				return false;
-			if(a[i][j-jj]==' ' || word.charAt(jj)==a[i][j-jj]) {
-				continue;
-			}
-			else
-				return false;
-		}
-		return true;
-		
-	}
+        return false;
+    }
     
+    boolean valid(char a[][],int i,int j,int n,int m,String str,int si){
+        if(i<0 || i>=n) return false;
+        if(j<0 || j>=m) return false;
+        return a[i][j]==' ' || a[i][j]==str.charAt(si);
+    }
     
-    // just going opposite direction of bottom
-    private static boolean canputup(char[][] a, String word, int i, int j) {
-	
-        if(i+1<=a.length-1 && a[i+1][j]!='#')
-			return false;
-		
-        else if(i-word.length()>=0 && a[i-word.length()][j]!='#')
-			return false;
-		
-        for(int ii=0;ii<word.length();ii++) {
-			if(i-ii<0)
-				return false;
-			if(a[i-ii][j]==' ' || word.charAt(ii)==a[i-ii][j]) {
-				continue;
-			}
-			else
-				return false;
-		}
-		return true;
-		
-	}
-    
-    //if you are reading this
-    //try it by using array named as directions
-    //left 0,-1
-    //down 1,0
-    //instead of similar 4 methonds
+    boolean validEnd(char a[][],int i,int j,int n,int m){
+        if(i<0 || i>=n) return true;
+        if(j<0 || j>=m) return true;
+        //ending pos
+        return a[i][j]=='#';
+    }
 }
