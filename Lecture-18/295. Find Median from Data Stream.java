5cf326b1ef115ec4c6b974b4a0989404c339d377
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2024-04-11 12:42:06 +0530

    Update 295. Find Median from Data Stream.java

--- Lecture-18/295. Find Median from Data Stream.java
+++ Lecture-18/295. Find Median from Data Stream.java
@@ -41,7 +41,69 @@ class MedianFinder {
     }
 }
 
+__________________________________________________________________________________________________________________________
+// Optimized
+
+
+
+
+class MedianFinder {
+    //min_pq represent right hand side
+    //max_pq represent left hand side
+    
+    //from left hand side we want higher value (max_pq)
+    //from right hand side we want lower value (min_pq)
+    
+    PriorityQueue<Integer> min, max;
+    public MedianFinder() {
+        //default
+        min = new PriorityQueue();
+        //max_converter using reverse_ordering
+        max = new PriorityQueue(Collections.reverseOrder());
+    }
+    
+    //O(1)
+    public void addNum(int num) {
+        if(min.size()==0 || num>min.peek()){
+            min.add(num);
+        }else{
+            max.add(num);
+        }
+        
+        //size balance out
+        if(min.size()-max.size()>1){
+            max.add( min.remove() );
+        }
+        
+        if(max.size()-min.size()>1){
+            min.add( max.remove() );
+        }
+        
+        
+    }
+    
+    //O(1)
+    public double findMedian() {
+        
+        //total_size if odd and min contains an extra value
+        if(min.size()>max.size())   return min.peek();
+        
+        //total_size if odd and max contains an extra value
+        if(max.size()>min.size())   return max.peek();
+        
+        
+        //size is even
+        return ( max.peek() + min.peek() )/2.0;
+        
+    }
+}
+
 /**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */;
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder obj = new MedianFinder();
  * obj.addNum(num);
