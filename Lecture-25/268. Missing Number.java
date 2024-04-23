2c13cc76744fbbe7c4dcc493556e01cc97fc13ab
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2024-04-24 02:32:02 +0530

    Create 268. Missing Number.java

---
+++ Lecture-25/268. Missing Number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0, i = 0;
+        for (i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor ^ i;
+    }
+}
